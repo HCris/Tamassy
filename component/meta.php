e55e5a40ee4b7fcda05f34dba64db29d45d08a7b
HCris <cristina.haruta95@gmail.com>
2017-10-11 12:51:56 +0300

    12.51

---
+++ component/meta.php
@@ -0,0 +1,5 @@
+	<meta charset="UTF-8">
+	<meta name="description" content="My portofolio">
+	<meta name="keywords" content="HTML,CSS,Bootstrap,JavaScript">
+	<meta name="author" content="H.C">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
