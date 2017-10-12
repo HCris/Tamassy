d5de960593793a9a927a03a5a81c7aef2a4d06eb
HCris <cristina.haruta95@gmail.com>
2017-10-12 16:26:16 +0300

    12.10.2017/16:26

--- component/navbar.php
+++ component/navbar.php
@@ -1,30 +1,34 @@
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+		<div class="row">
+			<div class="col col-md-12 "  style="padding :0px;">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNavDropdown">
+					<ul class="navbar-nav">
+						<li class="nav-item">
+							<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Features</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Pricing</a>
+						</li>
+					</ul>
+					<a class="navbar-brand" href="#" style="margin-left: 30%;">Navbar</a>
+				<ul class="navbar-nav" style="margin-left: 43%;">
+					<li class="nav-item ">
+						<a class="nav-link" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Features</a>
+						<a class="nav-link" href="#"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Pricing</a>
+						<a class="nav-link" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
 					</li>
 				</ul>
-				<a class="navbar-brand" href="#" style="margin-left: 30%;">Navbar</a>
-			<ul class="navbar-nav" style="margin-left: 43%;">
-				<li class="nav-item active">
-					<a class="nav-link" href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#"><i class="fa fa-github-alt" aria-hidden="true"></i></a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
-				</li>
-			</ul>
+			</div>
+			</nav>
+			</div>
 		</div>
-		</nav>
--- home.php
+++ home.php
@@ -10,36 +10,140 @@
 		<?php
 		 	include'component/navbar.php';
 		?>
-		<div class="row slaider">
-			<div class="col col-sm-12 col-md-12 col-lg-12 col-xl-12">
-				<div class="slideshow-container">
-
-					<div class="mySlides fade">
-						<div class="numbertext">1 / 3</div>
-						<img src="img/387130-bridges-architectural-wonders-around-the-world-jamsu-bridge-under-the-banpo-bridge-s-korea.jpg" style="width:100%">
-						<div class="text">Caption Text</div>
+		<div class="row" style="padding:0px">
+				<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner" role="listbox">
+						<div class="carousel-item active img">
+							<img class="d-block img-fluid" src="img/ahmed-hasan-331353.jpg" alt="First slide">
+						</div>
+						<div class="carousel-item img">
+							<img class="d-block img-fluid" src="img/ahmed-rizkhaan-306766.jpg" alt="Second slide">
+						</div>
+						<div class="carousel-item img">
+							<img class="d-block img-fluid" src="img/alexander-popov-367071.jpg" alt="Third slide">
+						</div>
 					</div>
-
-					<div class="mySlides fade">
-						<div class="numbertext">2 / 3</div>
-						<img src="img/ahmed-hasan-331353.jpg" style="width:100%">
-						<div class="text">Caption Two</div>
-					</div>
-
-					<div class="mySlides fade">
-						<div class="numbertext">3 / 3</div>
-						<img src="img/ahmed-rizkhaan-306766.jpg" style="width:100%">
-						<div class="text">Caption Three</div>
-					</div>
+					<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+					<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+					<span class="sr-only">Previous</span>
+					</a>
+					<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+					<span class="carousel-control-next-icon" aria-hidden="true"></span>
+					<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div class="row section_2_home_page">
+				<div class="col-md-6 col-md-offset-2">
+					<h1 class="underline" style="margin-top:0">Tamassy Creative</h1>
+					<h1 class="sub">London Digital Design Studio</h1>
 
 				</div>
-				<br>
+				<div class="col-md-6">
+					<p>Tamassy Creative is a digital media design studio based in London founded by a 	digital designer Taiyo Nagano.
+					</p>
+					<p>We offer design, branding, art direction and development across variety of digital platforms including web, mobile, print and motion graphics.
+					</p>
+					<p>Tamassy is an international network of talented designers and developers. We turn our passion for design and technology into commercial success for clients in business and public sector.
+					</p>
+					<p>
+						<a href="/about" class="btn btn-opposite">ABOUT US</a>
+						<a href="/services" class="btn btn-opposite">SERVICES</a>
+						<a href="/portfolios/all" class="btn btn-opposite">WORKS</a>
+					</p>
 
-				<div style="text-align:center">
-					<span class="dot"></span>
-					<span class="dot"></span>
-					<span class="dot"></span>
 				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col full-width-title dark-bg">
+				<h1>Featured Works</h1>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 section_4_home_page">
+				<div class="hovereffect">
+					<img class="img-responsive" src="img/alberto-restifo-95349.jpg" alt="">
+					<div class="overlay">
+						<h2>Effect 13</h2>
+						<p>
+							<a href="#">LINK HERE</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 section_4_home_page">
+				<div class="hovereffect">
+					<img class="img-responsive" src="img/alex-jodoin-246078.jpg" alt="">
+					<div class="overlay">
+						<h2>Effect 13</h2>
+						<p>
+							<a href="#">LINK HERE</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 section_4_home_page">
+				<div class="hovereffect">
+					<img class="img-responsive" src="img/ahmed-hasan-331353.jpg" alt="">
+					<div class="overlay">
+						<h2>Effect 13</h2>
+						<p>
+							<a href="#">LINK HERE</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row" style="margin-top:-100px;">
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 section_4_home_page">
+				<div class="hovereffect">
+					<img class="img-responsive" src="img/alberto-restifo-95349.jpg" alt="">
+					<div class="overlay">
+						<h2>Effect 13</h2>
+						<p>
+							<a href="#">LINK HERE</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 section_4_home_page">
+				<div class="hovereffect">
+					<img class="img-responsive" src="img/alex-jodoin-246078.jpg" alt="">
+					<div class="overlay">
+						<h2>Effect 13</h2>
+						<p>
+							<a href="#">LINK HERE</a>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 section_4_home_page">
+				<div class="hovereffect">
+					<img class="img-responsive" src="img/ahmed-hasan-331353.jpg" alt="">
+					<div class="overlay">
+						<h2>Effect 13</h2>
+						<p>
+							<a href="#">LINK HERE</a>
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row" style="margin-top: -100px;">
+			<div class="col full-width-title dark-bg">
+				<h1>Get in touch!</h1>
 			</div>
 		</div>
 		<?php
